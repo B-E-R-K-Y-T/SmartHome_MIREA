125a0f8b9fd1cf820611237f2cca3b95fcbd2fa2
B-E-R-K-Y-T <65071228+B-E-R-K-Y-T@users.noreply.github.com>
2022-01-17 18:01:58 +0300

    Update clients and add generate_sensors.py

--- sensors/generate_sensors.py
+++ sensors/generate_sensors.py
@@ -1,34 +1,60 @@
 import random
-from rich.progress import track
+import os
+import threading
+
+os.mkdir('Termo')
+os.mkdir('Light')
+os.mkdir('Water_out')
+os.mkdir('Open_door')
+os.mkdir('Work_TV')
+os.mkdir('BERKYT')
 
 sensors = ['Termo', 'Light', 'Water_out', 'Open_door', 'Work_TV', 'BERKYT']
 
-for i in track(range(1_000_000), description='   Создаю python-датчики...'):
-    name_sensor = random.choice(sensors) + '_' + str(i)
-    with open(f'{name_sensor}.py', 'w') as f:
-        f.write(
-            f'''
-# ======================================================================================================================
 
-# Author: BERKYT
-
-# ======================================================================================================================
-
-import client
-import random
-import time
-
-sensor = client.Client('127.0.0.1', 1234).get_client()
-
-while True:
-    bool_list = [True, False]
+def create_sensor(folder, sensor):
+    for i in range(10_000):
+        name_sensor = sensor + '_' + str(i)
+        with open(f'{folder}/{name_sensor}.py', 'w') as f:
+            f.write(
+                f'''
+    # ======================================================================================================================
     
-    if '{name_sensor}' != 'Termo{'_' + str(i)}':
-        sensor.send(('{name_sensor}: ' + str(random.choice(bool_list))).encode('utf-8'))
-        time.sleep(1)
-    else:
-        sensor.send(('{name_sensor}: ' + str(random.randint(1, 20))).encode('utf-8'))
-        time.sleep(1)
+    # Author: BERKYT
+    
+    # ======================================================================================================================
+    
+    import client
+    import random
+    import time
+    
+    sensor = client.Client('127.0.0.1', 1234).get_client()
+    
+    while True:
+        bool_list = [True, False]
+        
+        if '{name_sensor}' != 'Termo{'_' + str(i)}':
+            sensor.send(('{name_sensor}: ' + str(random.choice(bool_list))).encode('utf-8'))
+            time.sleep(1)
+        else:
+            sensor.send(('{name_sensor}: ' + str(random.randint(1, 20))).encode('utf-8'))
+            time.sleep(1)
+    
+    '''
+            )
 
-'''
+
+t = []
+for i in sensors:
+    t.append(
+        threading.Thread(
+            target=create_sensor,
+            args=(i, i)
         )
+    )
+
+    t[-1].start()
+
+
+
+
