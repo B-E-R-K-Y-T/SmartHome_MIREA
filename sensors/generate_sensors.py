c403b24a0ed46dcb361d4e4f312d667f58216640
B-E-R-K-Y-T <65071228+B-E-R-K-Y-T@users.noreply.github.com>
2022-01-17 16:55:57 +0300

    Update clients and add generate_sensors.py

--- sensors/generate_sensors.py
+++ sensors/generate_sensors.py
@@ -1,3 +1,12 @@
+import random
+
+sensors = ['Termo', 'Light', 'Water_out', 'Open_door', 'Work_TV', 'BERKYT']
+
+for i in range(100_000):
+    name_sensor = random.choice(sensors) + '_' + str(i)
+    with open(f'{name_sensor}.py', 'w') as f:
+        f.write(
+            f'''
 # ======================================================================================================================
 
 # Author: BERKYT
@@ -11,5 +20,7 @@ import time
 sensor = client.Client('127.0.0.1', 1234).get_client()
 
 while True:
-    sensor.send(('Termo: ' + str(random.randint(1, 20))).encode('utf-8'))
+    sensor.send(('{name_sensor}_sensor: ' + str(random.randint(1, 20))).encode('utf-8'))
     time.sleep(1)
+'''
+        )
