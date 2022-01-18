b54fc8a352fcca2ef0f445399f8ee9f876ef0907
B-E-R-K-Y-T <65071228+B-E-R-K-Y-T@users.noreply.github.com>
2022-01-18 03:26:49 +0300

    init's files

--- bot_controller/bot_controller.py
+++ bot_controller/bot_controller.py
@@ -8,16 +8,17 @@ import logging
 import socket
 import asyncio
 import aiohttp
+import json
 
 from aiogram import Bot, Dispatcher, executor, types
 from other.config import TOKEN
 
 
-# client = socket.socket(
-#     socket.AF_INET,
-#     socket.SOCK_STREAM
-# )
-# client.connect(('127.0.0.1', 1234))
+client = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+client.connect(('127.0.0.1', 1234))
 
 API_TOKEN = TOKEN
 
@@ -38,6 +39,17 @@ async def send_welcome(message: types.Message):
     This handler will be called when user sends `/start` or `/help` command
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(command='off')
+async def echo(message: types.Message):
+
+    with open('personal.json', 'r', encoding='utf-8') as f:  # открыли файл
+        text = json.load(f)  # загнали все из файла в переменную
+        print(text)  # вывели результат на экран
+
+    await message.answer(text['mode'])
+    await message.answer(message.chat.id)
 
 
 @dp.message_handler()
